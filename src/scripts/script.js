2d7920bb7733af55058a2f7afcf0912bb396afa8
Vadim <mr.kostura@mail.ru>
2025-03-17 21:19:32 +0300

    Состояние ui-элементов

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,17 +2,23 @@ document.addEventListener('DOMContentLoaded', () => {
 	console.log('GitHub nickname:', 'VadimNest');
 
 	const form = document.querySelector('.form-page__content');
+	const submitButton = form.querySelector('.form-page__submit-button');
+	const nameInput = form.querySelector('#name');
 
 	form.addEventListener('submit', (event) => {
 		event.preventDefault();
 
-		const nameInput = form.querySelector('#name');
-		const nameValue = nameInput.value;
+		submitButton.removeAttribute('data-success');
+		nameInput.removeAttribute('data-error');
+
+		const nameValue = nameInput.value.trim();
 
 		if (nameValue) {
 			console.log('Форма отправлена. Введенное имя:', nameValue);
+			submitButton.setAttribute('data-success', '');
 		} else {
 			console.log('Поле имени пустое!');
+			nameInput.setAttribute('data-error', '');
 		}
 
 		form.reset();
